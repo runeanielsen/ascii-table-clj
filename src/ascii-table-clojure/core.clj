58b1ada6242ad43094d006446d571a19fcde5cff
Rune Nielsen <runenielsen@runbox.com>
2022-04-03 19:58:11 +0200

    improves readability

--- src/ascii-table-clojure/core.clj
+++ src/ascii-table-clojure/core.clj
@@ -1,32 +1,32 @@
 (ns ascii-table-clojure.core
   (:require [clojure.string :as str]))
 
-(def ^:private header
+(def header
   (->> "Dec  Hex  Oct  C"
        (repeat 4)
        (str/join " | ")))
 
-(def ^:private table-rows
+(def table-rows
   (for [i (range 32)]
     (map #(+ i (* 32 %)) (range 4))))
 
-(defn- get-display-char [n]
+(defn get-display-char [n]
   (if (or (< n 33) (== n 127))
     \space
     (char n)))
 
-(defn- format-block [n]
+(defn format-block [n]
   (format "%3d %4o %4x %2c" n n n (get-display-char n)))
 
-(defn- format-row [xs]
+(defn format-row [xs]
   (->> xs
        (map format-block)
        (str/join " | ")))
 
 (defn ascii-table []
-  (str header \newline
-       (->> table-rows
-            (map format-row)
-            (str/join \newline))))
+  (str/join
+   \newline
+   (conj (map format-row table-rows) header)))
 
-(println (ascii-table))
+(comment
+  (println (ascii-table)))
